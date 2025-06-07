ac3425905b1891d6f3b0f5d98a638df8d2da79a1
Aman <theaman1805@gmail.com>
2025-06-07 15:52:27 +0530

    removed migrations

--- hotel/firstpage/context_processors.py
+++
@@ -1,7 +0,0 @@
-from .models import NewUpdate
-
-def new_updates_processor(request):
-    new_updates = NewUpdate.objects.all().order_by('-date')
-    return {
-        'base_new_updates': new_updates
-    }
