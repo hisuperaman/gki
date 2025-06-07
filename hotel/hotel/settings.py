aec1b74a7f9187efe628693857cc49f55ac34814
Aman <theaman1805@gmail.com>
2025-06-07 16:01:07 +0530

    new

--- hotel/hotel/settings.py
+++ hotel/hotel/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-l_$9mp8vk5@g#$_nbc-churpa6_@ql2z0eexc9d3x$_(8z8o@&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
